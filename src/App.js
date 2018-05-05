1e9e690a7688a6d18bbef7bf9f35d346d0aacb6b
Tanisha <tanishahampden@gmail.com>
2018-05-05 13:33:32 -0700

    add logo image and title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import lashunta from './lashunta.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={lashunta} className="App-logo" alt="logo" />
+          <h1 className="App-title">The Lagunta App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
